a5398e0cd489684b21ecd01100082b3c543d2ae9
Olena Serhata <olena.serhata@pitchbook.com>
2024-01-11 18:51:58 +0200

    update pom files, add page provider class, add base test class, add libs and resources

---
+++ src/main/java/libs/TestData.java
@@ -0,0 +1,6 @@
+package libs;
+
+public class TestData {
+    public final static String EXIST_USER_EMAIL = "sergataalenka@gmail.com";
+    public final static String EXIST_USER_PASSWORD = "test0211";
+}
